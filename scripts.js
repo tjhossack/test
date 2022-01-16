b5883b4a55906ac129cd3ca446fc740bf4eb2eeb
tjhossack <77854898+tjhossack@users.noreply.github.com>
2022-01-16 18:13:32 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+
+
+    cell1.innerHTML = "x";
+    cell2.innerHTML = "x";
+    cell3.innerHTML = "x";
+    cell4.innerHTML = "x";
+    cell5.innerHTML = "x";
+    cell6.innerHTML = "x";
+  }
